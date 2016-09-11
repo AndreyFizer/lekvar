34ed005b010b085e04d4af54b1f3a49865df3ec1
AndreyFizer <fizer.ap@outlook.com>
2016-09-11 14:55:20 +0300

    some changes

--- public/source/js/vendors.js
+++ public/source/js/vendors.js
@@ -1,7 +1,12 @@
-import _ from  'lodash';
-import Backbone from 'backbone';
-import $ from 'jquery';
-import Marionette from 'backbone.marionette';
+// import _ from  './libs/underscore/underscore-min';
+// import Backbone from './libs/backbone/backbone-min';
+// import $ from './libs/jquery/dist/jquery.min';
+// import Marionette from './libs/backbone.marionette/lib/backbone.marionette.min';
+
+const _ = require('./libs/underscore/underscore-min');
+const Backbone = require('./libs/backbone/backbone-min');
+const $ = require('./libs/jquery/dist/jquery.min');
+const Marionette = require('./libs/backbone.marionette/lib/backbone.marionette.min');
 
 Backbone.$ = $;
 window.$ = $;
