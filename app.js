49bd84013ce01fa7ee32b70b3b40c16c76e50752
AndreyFizer <fizer.ap@outlook.com>
2016-12-08 17:59:29 +0200

    map data

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ db.once('open', function () {
     
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.header("Access-Control-Allow-Methods", req.method);
         next();
     });
