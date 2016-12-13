710545199afcbad085df9a51d163f4aae34f2b79
AndreyFizer <fizer.ap@outlook.com>
2016-12-13 10:46:49 +0200

    map data

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,15 @@ var _eventsCache = [
     {id: 5, coordinates: [28, -30], class: 'A', description: 'good morning'},
     {id: 6, coordinates: [30, 23], class: 'C', description: 'antananarivoo'}
 ];
+
+var _linksCache = [
+    {path: [1, 2], kind: 'trip'},
+    {path: [4, 2], kind: 'trip'},
+    {path: [2, 5, 6], kind: 'horse trip'},
+    {path: [4, 5], kind: 'just_line'},
+    {path: [1, 2, 5], kind: 'horse trip'}
+];
+
 var _eventId = 6;
 
 module.exports = function(app, db) {
@@ -33,24 +42,20 @@ module.exports = function(app, db) {
         const result = {
             nodes: [
                 {id: 0, name: 'Peter', url: 'http://digmast.ru/images/lessons/photo_for_documents/preview.jpg'},
-                {
-                    id  : 1,
+                {id     : 1,
                     name: 'Paul',
                     url : 'https://dreamitcodeitwinit.files.wordpress.com/2014/01/290053v2-max-250x250.jpg?w=1000'
                 },
-                {
-                    id  : 2,
+                {id     : 2,
                     name: 'Florian',
                     url : 'http://gazeta.lviv.ua/wp-content/uploads/2016/06/12910642_584264068395305_278364554_n-250x250-1-1-1-1-2.jpg'
                 },
-                {
-                    id  : 3,
+                {id     : 3,
                     name: 'Thomas',
                     url : 'http://golbis.com/wp-content/uploads/2016/09/1473427482k84ng-250x250.jpg'
                 },
                 {id: 4, name: 'Anna', url: 'https://active-camp.ru/wp-content/uploads/2015/10/avatar-250x250.jpg'},
-                {
-                    id  : 5,
+                {id     : 5,
                     name: 'Julia',
                     url : 'http://devushka.ru/upload/pagestyle/55_avatar_Ruslana%20%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80.jpg'
                 },
@@ -88,7 +93,8 @@ module.exports = function(app, db) {
     
     app.get('/mapdata', function(req, res, next) {
         const result = {
-            events: _eventsCache
+            events: _eventsCache,
+            links : _linksCache
         };
         
         // res.setHeader("Access-Control-Allow-Origin", "*");
